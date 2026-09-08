104d77a77a6277078073e69b2ca68dd70d8fb8d9
Maria Fernanda Bighi Siqueira <maria.f.siqueira8@aluno.senai.br>
2026-09-08 15:36:51 -0300

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,71 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <DHT.h>
 
-// Configura o endereço do LCD para 0x27 com 16 colunas e 2 linhas
-// Nota: Se o texto não aparecer, mude o endereço para 0x3F
+#define DHTPIN 15
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
-  // Inicializa o display LCD
+  Serial.begin(115200);
+  dht.begin();
   lcd.init();
-
-  // Liga a luz de fundo (backlight)
   lcd.backlight();
 
-  // Define o cursor na Coluna 0, Linha 0 (Primeira linha)
   lcd.setCursor(0, 0);
-  lcd.print("Fer Bighi"); // <-- Substitua pelo seu nome real
-
-  // Define o cursor na Coluna 0, Linha 1 (Segunda linha)
-  lcd.setCursor(0, 1);
-  lcd.print("IoT SENAI-SP");
+  lcd.print("Iniciando...");
+  delay(2000);
+  lcd.clear();
 }
 
 void loop()
 {
-  // Código estático, não precisa rodar nada repetidamente no loop
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t))
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("Erro no sensor ");
+    return;
+  }
+
+  // ALERTA: Se a temperatura passar de 30°C
+  if (t > 30.0)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("   !!ALERTA!!   ");
+    lcd.setCursor(0, 1);
+    lcd.print("Temp alta: ");
+    lcd.print(t, 1);
+    lcd.print("C ");
+
+    // Faz a luz de fundo piscar
+    lcd.noBacklight();
+    delay(500);
+    lcd.backlight();
+    delay(500);
+  }
+  else
+  {
+    // Funcionamento Normal (Garante que a luz fique acesa)
+    lcd.backlight();
+
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(t, 1);
+    lcd.print((char)223); // Símbolo de grau °
+    lcd.print("C    ");
+
+    lcd.setCursor(0, 1);
+    lcd.print("Umid: ");
+    lcd.print(h, 1);
+    lcd.print(" %   ");
+
+    delay(2000);
+  }
 }
