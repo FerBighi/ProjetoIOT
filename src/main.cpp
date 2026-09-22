00f5e29a0223a5b0cf0e956c317ff9327d52881a
Maria Fernanda Bighi Siqueira <maria.f.siqueira8@aluno.senai.br>
2026-09-22 13:32:38 -0300

    change

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ const char *ssid = "Irineu";
 const char *password = "12345678";
 
 // API ViaCEP em HTTP direto (Porta 80 - sem redirecionamento 308/301)
-const char *serverUrlGET = "http://viacep.com.br/ws/01001000/json/";
+const char *serverUrlGET = "http://viacep.com.br/ws/17704602/json/";
 
 void setup()
 {
